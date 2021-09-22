fa1a845c57cc8315a293deaab5f14b7cde8a7e8c
thawatchairst <minirst26@gmail.com>
2021-09-21 22:42:12 -0700

    deploy chang config5

--- CoreProject/urls.py
+++ CoreProject/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from webCovid import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

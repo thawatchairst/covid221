b54d6c3a11a5ce12852db78f961d9106226cf3b6
thawatchairst <minirst26@gmail.com>
2021-09-21 23:19:39 -0700

    deploy chang config88

--- webCovid/views.py
+++ webCovid/views.py
@@ -6,10 +6,10 @@ import requests
 
 
 def index(request):
-    req = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all')
-    req_po = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces')
-    jsonData_Po =req_po.json()
-    jsonData = req.json()
-    Statuscode =req.status_code
-    #return render(request ,'index.html')
-    return render(request ,'index.html',{'status':Statuscode,'data':jsonData,'data_po':jsonData_Po})
+    #req = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all')
+    #req_po = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces')
+    #jsonData_Po =req_po.json()
+    #jsonData = req.json()
+    #Statuscode =req.status_code
+    return render(request ,'index.html')
+    #return render(request ,'index.html',{'status':Statuscode,'data':jsonData,'data_po':jsonData_Po})

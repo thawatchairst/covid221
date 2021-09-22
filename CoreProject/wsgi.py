44ed279522cae24b8fdbdd7984d588634f202008
thawatchairst <minirst26@gmail.com>
2021-09-21 22:07:22 -0700

    deploy chang config4

--- CoreProject/wsgi.py
+++ CoreProject/wsgi.py
@@ -18,4 +18,4 @@ import os
 from django.core.wsgi  import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','djangobasic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','webCovid.settings')

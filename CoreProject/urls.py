a8b171fe007cd1546562f13e330955c07148352d
thawatchairst <minirst26@gmail.com>
2021-09-21 22:48:45 -0700

    deploy setting3

--- CoreProject/urls.py
+++ CoreProject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from webCovid import views
 from django.conf.urls.static import static
+from djnago.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),

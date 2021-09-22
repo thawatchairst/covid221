a9d8ed1e5328823595cdf38ee3d911debd7a4e98
thawatchairst <minirst26@gmail.com>
2021-09-21 21:54:14 -0700

    deploy chang config

--- CoreProject/wsgi.py
+++ CoreProject/wsgi.py
@@ -15,3 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoreProject.settings')
 
 application = get_wsgi_application()
 import os
+from django.core.wsgi  import get_wsgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','djangobasic.settings')

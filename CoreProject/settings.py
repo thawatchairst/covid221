5243818b18f3e93d96a1ff64a3f4199bf9f3d789
thawatchairst <minirst26@gmail.com>
2021-09-21 23:04:59 -0700

    deploy chang config888

--- CoreProject/settings.py
+++ CoreProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#&4_yptrc(8i2+g9n=f-xd5p9$k_9j@_w$sj+)b@&rnn&t0u&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

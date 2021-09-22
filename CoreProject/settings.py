abf128d1502d98ab2bb2f4ab8685eb724fdd5d71
thawatchairst <minirst26@gmail.com>
2021-09-21 22:37:47 -0700

    deploy chang config4

--- CoreProject/settings.py
+++ CoreProject/settings.py
@@ -131,5 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

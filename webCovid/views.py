c3a425da33fa9416da330bfb2fb9ad25b43389ae
thawatchairst <minirst26@gmail.com>
2021-09-21 23:33:51 -0700

    deploy chang config Jayyy22

--- webCovid/views.py
+++ webCovid/views.py
@@ -10,12 +10,14 @@ def formatdate(time1):
             if x =='update_date':
                 date1 =y
     print(date1)
+    return date1
 
 def index(request):
     req = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all')
     req_po = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces')
     jsonData_Po =req_po.json()
     jsonData = req.json()
+    formatdate(jsonData)
     Statuscode =req.status_code
     #return render(request ,'index.html')
     return render(request ,'index.html',{'status':Statuscode,'data':jsonData,'data_po':jsonData_Po})
